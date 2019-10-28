a4fd2f5ab681b831d935a8773484a0c60759604e
patrick-gordon <patrickgordon42@yahoo.com>
2019-10-28 13:37:59 -0500

    personal dash outline and some styling

--- src/views/personal_dashboard/PersonalDashboard.js
+++ src/views/personal_dashboard/PersonalDashboard.js
@@ -1,9 +1,46 @@
 import React from 'react'
+import './PersonalDashboard.scss'
 
 export default function PersonalDashboard() {
     return (
-        <div>
-            <h1>PersonalDashboard</h1>
-        </div>
+        <>
+        <header>
+            <h1>Sam Wilsons Impact</h1>
+        </header>
+        <main>
+            <div className='card1'>
+                <div className='left'>
+                    <p>Children Served</p>
+                    <p> Your total:</p>
+                </div>
+               
+                <div className='right'>
+                    <p>hhhhh</p>
+                </div>
+            </div>
+
+            <div className='card2'>
+                <div className='left'>
+                    <p>Connections Discovered</p>
+                    <p> Your total:</p>
+                </div>
+               
+                <div className='right'>
+                    <p>hhhhh</p>
+                </div>
+            </div>
+            
+            <div className='card3'>
+                <div className='left'>
+                    <p>Engagements Events</p>
+                    <p> Your total:</p>
+                </div>
+               
+                <div className='right'>
+                    <p>hhhhh</p>
+                </div>
+            </div>
+        </main>
+        </>
     )
 }
