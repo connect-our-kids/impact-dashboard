71795315e81de703cb7975adae13193665f80cb0
Matt Herich <mjherich@gmail.com>
2019-10-28 13:47:05 -0700

    Add navigation for logged in user

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 import './index.scss';
 
 import NavBar from './components/NavBar';
-import Profile from './components/Profile';
 import PublicDashboard from './views/public_dashboard/PublicDashboard';
 import PersonalDashboard from './views/personal_dashboard/PersonalDashboard';
 import TeamDashboard from './views/team_dashboard/TeamDashboard';
@@ -19,7 +18,6 @@ function App() {
   return (
     <div className="App">
       <NavBar/>
-      <Profile/>
       <Switch>
         <Route exact path='/' component={PublicDashboard}/>
         <PrivateRoute path='/team' component={TeamDashboard} />
