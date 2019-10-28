bc9070e4bfccecc96ef639c30c39e05c76d2da34
Matt Herich <mjherich@gmail.com>
2019-10-28 14:10:32 -0700

    Add tempData and update metric map

--- src/views/public_dashboard/PublicDashboard.js
+++ src/views/public_dashboard/PublicDashboard.js
@@ -4,6 +4,33 @@ import { increment, decrement } from '../../redux/actions/index';
 import {connect} from 'react-redux';
 import styled from 'styled-components';
 
+const tempData = [
+  {
+    metric: "Children Served",
+    value: "5,236"
+  },
+  {
+    metric: "Successful Placement Ratio",
+    value: "82%"
+  },
+  {
+    metric: "Connections Discovered",
+    value: "64,234"
+  },
+  {
+    metric: "Kinship Search Users",
+    value: "1,865"
+  },
+  {
+    metric: "Permanent Placements",
+    value: "4,634"
+  },
+  {
+    metric: "Average Days To Placement",
+    value: "34"
+  },
+]
+
 const PublicDashboard = (props) => {
 
   const Button = styled.button`
@@ -30,16 +57,22 @@ const PublicDashboard = (props) => {
     <>
     <>
       <header>
-        <h1>Public Dashboard</h1>
-        <h2>Check out how our efforts are making an impact:</h2>
+        <h2 className="header-callout">Check out how our efforts are making an impact:</h2>
       </header>
       <main>
         <div className="public-stats-grid">
           {/* provided words is not undefined (after the fetch above happens), map over the first 6 words, displaying a div with the wordcount and word */}
-          {words ? words.slice(0,6).map((word, idx) => <div key={idx} className="metric">
+          {/* {words ? words.slice(0,6).map((word, idx) => <div key={idx} className="metric">
             <b>{word.word_count}</b>
             <p>{word.word}</p>
-          </div>) : 'Loading words'}
+          </div>) : 'Loading words'} */}
+          { tempData.map((el, idx) => (
+            <div key={idx} className={`metric metric-${idx}`}>
+              <b>{el.value}</b>
+              <div className="divider"/>
+              <p>{el.metric}</p>
+            </div>
+          )) }
         </div>
       </main>
 
