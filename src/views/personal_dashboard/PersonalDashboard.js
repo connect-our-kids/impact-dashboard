77f031e7e2f6230d62518780b3fa4f1a46efb0ae
patrick-gordon <patrickgordon42@yahoo.com>
2019-10-29 11:56:41 -0500

    # Description                              added personal dash and all styling to go with it                                                        ## Type of change - [X] New feature (non-breaking change which adds functionality) ## Change status - [X] Complete, tested, ready to review and merge # Checklist: - [X ] I have pulled from master before submitting this PR - [X] My code follows the style guidelines of this project - [X ] I have performed a self-review of my own code - [X ] I have commented my code, particularly in hard-to-understand areas - [X ] I have made corresponding changes to the documentation - [X ] My changes generate no new warnings - [X ] I have added tests that prove my fix is effective or that my feature works - [X ] New and existing unit tests pass locally with my changes - [X ] There are no merge conflicts - [X ] Any env variables or config options that are needed have been stated in the README - [] My code has been reviewed by at least one peer (the peer review to approve a PR counts.  The reviewer must download and test the code) - [X] NO API KEYS ARE PUSHED UP

--- src/views/personal_dashboard/PersonalDashboard.js
+++ src/views/personal_dashboard/PersonalDashboard.js
@@ -1,46 +1,68 @@
 import React from 'react'
 import './PersonalDashboard.scss'
+import styled from 'styled-components';
 
 export default function PersonalDashboard() {
+    
+    const Button = styled.button`
+    height: 3rem;
+    width: 10rem;
+    background-color: #1d8eb6;
+    color: white;
+    font-size: 20px;
+    margin-left: 33rem;
+    margin-top: 1rem;
+    `;
+
     return (
         <>
         <header>
             <h1>Sam Wilsons Impact</h1>
+            <Button>Share My Impact</Button>
         </header>
         <main>
             <div className='card1'>
                 <div className='left'>
-                    <p>Children Served</p>
+                    <h3>Children Served</h3>
                     <p> Your total:</p>
+                    <p>Next Threshold:</p>
+
                 </div>
                
                 <div className='right'>
-                    <p>hhhhh</p>
+                    <img src='Badge1.svg'></img>
                 </div>
             </div>
 
             <div className='card2'>
                 <div className='left'>
-                    <p>Connections Discovered</p>
+                    <h3>Connections Discovered</h3>
                     <p> Your total:</p>
+                    <p>Next Threshold:</p>
+
                 </div>
                
                 <div className='right'>
-                    <p>hhhhh</p>
+                <img src='/Badge2.svg'></img>
                 </div>
             </div>
-            
+
             <div className='card3'>
                 <div className='left'>
-                    <p>Engagements Events</p>
+                    <h3>Engagements Events</h3>
                     <p> Your total:</p>
+                    <p>Next Threshold:</p>
                 </div>
                
                 <div className='right'>
-                    <p>hhhhh</p>
+                    <img src='/Badge3.svg'></img>
                 </div>
             </div>
         </main>
+
+        <div className='text'>
+            <p>Thank you for being a super hero to kids in foster care. Your work is important and worth sharing. Now you can share your impact with the world. Encourage your colleagues to do the same! </p>
+        </div>
         </>
     )
 }
