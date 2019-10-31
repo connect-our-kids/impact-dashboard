f994edcbe53a6a82461f7a71629c4f213ad62445
Sean Wu <seanwu20@gmail.com>
2019-10-31 14:00:20 -0400

    working

--- lambda/ui.js
+++ lambda/ui.js
@@ -19,10 +19,10 @@ const binaryMimeTypes = [
   'text/xml',
   'image/x-icon',
   'image/svg+xml',
-    'application/x-font-ttf'
+  'application/x-font-ttf'
 ];
 
 //server.app.use(awsServerlessExpressMiddleware.eventContext());
 
-const serverProxy = awsServerlessExpress.createServer(server.app, null, binaryMimeTypes);
+const serverProxy = awsServerlessExpress.createServer(server, null, binaryMimeTypes);
 module.exports.universal = (event, context) => awsServerlessExpress.proxy(serverProxy, event, context);
