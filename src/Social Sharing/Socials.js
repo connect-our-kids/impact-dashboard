e05af9c3cb6eef724c8c0ca71dca3692d6e73433
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-11-06 21:32:48 -0500

    social share working, figuring out how to pass badge though

---
+++ src/Social Sharing/Socials.js
@@ -0,0 +1,88 @@
+
+import React from "react";
+import "./Socials.scss";
+import {
+  FacebookShareButton,
+  LinkedinShareButton,
+  TwitterShareButton,
+  EmailShareButton
+} from "react-share";
+
+import {
+  FacebookIcon,
+  TwitterIcon,
+  LinkedinIcon,
+  EmailIcon
+} from "react-share";
+
+export default function Socials(props) {
+  return (
+    <div className="social__sharing">
+      <div className="social_media">
+        <FacebookShareButton
+          url={props.url}
+          className="social__media--buttonFB"
+        >
+          <p className="socials_name">Facebook</p>
+          <FacebookIcon
+            size={30}
+            round={true}
+            logoFillColor={"black"}
+            iconBgStyle={{ fill: "white" }}
+          />
+        </FacebookShareButton>
+      </div>
+      <div className="social_media">
+        <LinkedinShareButton
+          url={props.url}
+          windowWidth={750}
+          windowHeight={600}
+          className="social__media--button"
+        >
+          <p className="socials_name">Linkedin</p>
+
+          <LinkedinIcon
+            size={30}
+            round={true}
+            logoFillColor={"black"}
+            iconBgStyle={{ fill: "white" }}
+          />
+        </LinkedinShareButton>
+      </div>
+      <div className="social_media">
+        <TwitterShareButton
+          url={props.url}
+          windowWidth={750}
+          windowHeight={600}
+          className="social__media--button"
+        >
+          <p className="socials_name">Twitter</p>
+          <TwitterIcon
+            size={30}
+            round={true}
+            logoFillColor={"black"}
+            iconBgStyle={{ fill: "white" }}
+          />
+        </TwitterShareButton>
+      </div>
+      <div className="social_media">
+        <EmailShareButton
+          url={props.url}
+          subject="This is a cool subject"
+          body={`Hey there plz share my link: ${props.url}`}
+          windowWidth={750}
+          windowHeight={600}
+          className="social__media--button"
+        >
+          <p className="socials_name">Email</p>
+          <EmailIcon
+            size={30}
+            round={true}
+            logoFillColor={"black"}
+            iconBgStyle={{ fill: "white" }}
+          />
+        </EmailShareButton>
+      </div>
+    </div>
+  );
+}
--- src/views/personal_dashboard/PersonalDashboard.js
+++ src/views/personal_dashboard/PersonalDashboard.js
@@ -1,83 +1,125 @@
-import React, { useState, useContext } from 'react'
-import { Facebook, Twitter, Linkedin } from 'react-social-sharing'
-import Modal from 'react-modal'
-import { Auth0Context } from '../../auth0-wrapper'
-
-import './PersonalDashboard.scss'
+//added in modal for social sharing
+import React, { useState } from "react";
+import "./PersonalDashboard.scss";
+import Modal from "react-modal";
+import Socials from "../../Social Sharing/Socials";
+import ShareIcon from "../../Social Sharing/share.png";
 
 export default function PersonalDashboard() {
-  const { user } = useContext(Auth0Context);
   const [isOpen, setIsOpen] = useState(false);
-
   const toggleModal = () => {
     setIsOpen(!isOpen);
-  }
+  };
+  
+
   return (
     <>
-      <header className='personal'>
-        <h1 className='personal__title'>Sam Wilsons Impact</h1>
-        <a className='personal__share'>Share My Impact</a>
-      </header>
-      <Modal isOpen={isOpen} parentSelector={() => document.body} className="Modal" >
-        <p>Modal Content.</p>
-        <button onClick={toggleModal}>Close</button>
+      <Modal
+        isOpen={isOpen}
+        parentSelector={() => document.body}
+        className="Modal"
+      >
+        <Socials url="www.connectourkid.org"
+                  text="Check out this website to learn more about this organization" />
+        <button className="modal-close" onClick={toggleModal}>
+          Close
+        </button>
       </Modal>
-      <div className='personal__main'>
-        <div className='personal__card'>
-          <div className='personal__card--left'>
+      <header className="personal">
+        <h1 className="personal__title">Sam Wilsons Impact</h1>
+        <a className="personal__share">Share My Impact</a>
+      </header>
+      <div className="personal__main">
+        <div className="personal__card1">
+          <div className="personal__card1--left">
             <h3>Children Served</h3>
-            <p> Your total: <strong>500</strong></p>
-            <p>Next Threshold: <strong>700</strong></p>
+            <p>
+              {" "}
+              Your total: <strong>500</strong>
+            </p>
+            <p>
+              Next Threshold: <strong>700</strong>
+            </p>
           </div>
 
-          <div className='personal__card--right'>
-            <img className="badgeIcon" src='Badge1.svg' alt='children served icon' />
-            <div className="shareIcon">
-              <img src="icons8-share.svg" alt="Share Icon" title="Share" onClick={toggleModal} />
+          <div className="personal__card1--right">
+            <img src="Badge1.svg" alt="children served icon"></img>
+            <div className="personal__card1--shareicon">
+              <p className="personal__card1--p">Share</p>
+              <img
+                src={ShareIcon}
+                alt="share icon"
+                className="personal__card1--share"
+                title="Share"
+                onClick={toggleModal}
+              ></img>
             </div>
           </div>
         </div>
 
-        <div className='personal__card'>
-          <div className='personal__card--left'>
+        <div className="personal__card2">
+          <div className="personal__card2--left">
             <h3>Connections Discovered</h3>
-            <p> Your total: <strong>100</strong></p>
-            <p>Next Threshold: <strong>700</strong></p>
+            <p>
+              {" "}
+              Your total: <strong>100</strong>
+            </p>
+            <p>
+              Next Threshold: <strong>700</strong>
+            </p>
           </div>
 
-          <div className='personal__card--right'>
-            <img className="badgeIcon" src='/Badge2.svg' alt='connections discovered icon' />
-            <div className="shareIcon">
-              <img src="icons8-share.svg" alt="Share Icon" title="Share" onClick={toggleModal} />
+          <div className="personal__card2--right">
+            <img src="/Badge2.svg" alt="connections discovered icon"></img>
+            <div className="personal__card2--shareicon">
+              <p className="personal__card2--p">Share</p>
+              <img
+                src={ShareIcon}
+                alt="share icon"
+                className="personal__card2--share"
+                title="Share"
+                onClick={toggleModal}
+              ></img>
             </div>
           </div>
         </div>
 
-        <div className='personal__card'>
-          <div className='personal__card--left'>
+        <div className="personal__card3">
+          <div className="personal__card3--left">
             <h3>Engagements Events</h3>
-            <p> Your total: <strong>20</strong></p>
-            <p>Next Threshold: <strong>70</strong></p>
+            <p>
+              {" "}
+              Your total: <strong>20</strong>
+            </p>
+            <p>
+              Next Threshold: <strong>70</strong>
+            </p>
           </div>
 
-          <div className='personal__card--right'>
-            <img className="badgeIcon" src='/Badge3.svg' alt='engagemnets events icon' />
-            <div className="shareIcon">
-              <img src="icons8-share.svg" alt="Share Icon" title="Share" onClick={toggleModal} />
+          <div className="personal__card3--right">
+            <img src="/Badge3.svg" alt="engagemnets events icon"></img>
+            <div className="personal__card3--shareicon">
+              <p className="personal__card3--p">Share</p>
+              <img
+                src={ShareIcon}
+                alt="share icon"
+                className="personal__card3--share"
+                title="Share"
+                onClick={toggleModal}
+              ></img>
             </div>
           </div>
         </div>
       </div>
 
-      <div className="sharing">
-        <h2>React Social Sharing</h2>
-        <Facebook link="https://google.com" small />
-        <Twitter link="https://google.com" small />
-        <Linkedin link="https://google.com" small />
-      </div>
-      <div className='personal__bottomtext'>
-        <p><strong>Thank you</strong> for being a super hero to kids in foster care. Your work is important and worth sharing. Now you can <strong>share your impact</strong> with the world. Encourage your colleagues to do the same! </p>
+      <div className="personal__bottomtext">
+        <p>
+          <strong>Thank you</strong> for being a super hero to kids in foster
+          care. Your work is important and worth sharing. Now you can{" "}
+          <strong>share your impact</strong> with the world. Encourage your
+          colleagues to do the same!{" "}
+        </p>
       </div>
     </>
-  )
+  );
 }
