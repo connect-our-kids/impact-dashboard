198fb24882d2354c3a3825aa44e9ba5fb1f217c2
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-11-13 12:37:10 -0500

    working on public dash sata impl

--- src/Social Sharing/Socials.js
+++ src/Social Sharing/Socials.js
@@ -52,7 +52,7 @@ export default function Socials(props) {
       <div className="social_media">
         <TwitterShareButton
           url={props.url}
-          title={props.text}
+          title="check out this! https://bv9cpgqr4l.execute-api.us-east-1.amazonaws.com/dev-nisa/Badges "
           windowWidth={750}
           windowHeight={600}
           className="social__media--button"
