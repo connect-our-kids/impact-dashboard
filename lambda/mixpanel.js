d7e8f82fa927d2aa92eb32cd05296354b08727d2
Desiree Morris <desiquinncompany@gmail.com>
2019-11-18 15:09:42 -0500

    changed names of functions to match with future changes

--- lambda/mixpanel.js
+++ lambda/mixpanel.js
@@ -5,7 +5,15 @@ const mixpanel = require("mixpanel-browser");
 //mixpanel.init("YOUR_TOKEN");
 //mixpanel.track("An event");
 
-exports.mixpanelSend = async () => {
+exports.sendEvent = async () => {
+
+    //initialize mixpanel with the given token (place into a secrets file)
+    mixpanel.init("token here");
+
+
+}
+
+exports.sendUserInfo = async () => {
 
     //initialize mixpanel with the given token (place into a secrets file)
     mixpanel.init("token here");
