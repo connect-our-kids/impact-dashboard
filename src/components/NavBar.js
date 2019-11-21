979db1d4f36d9539b1869a55a1c92e0ecdc1c879
Matt Herich <mjherich@gmail.com>
2019-11-21 10:10:19 -0800

    Update navbar with connectourkids.org homepage link and username

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
-import React, { useContext } from "react";
+import React from "react";
 import { Link, NavLink } from "react-router-dom";
-import { useAuth0, Auth0Context } from "../auth0-wrapper";
+import { useAuth0 } from "../auth0-wrapper";
 
 import './NavBar.scss';
 import { redirectUri } from '../index';
@@ -15,14 +15,14 @@ const LoggedInNav = () => (
   </div>
 )
 
-const LoggedInDropdown = ({ open }) => {
-  const { logout } = useAuth0();
+const LoggedInDropdown = ({ open, logout, user }) => {
   return (
     <div className={`nav__loggedInDropdown${open ? " open" : ""}`}>
       <ul>
         {/* Family Connections is not yet available but it should be added to this list when it comes out mid 2020 */}
         {/* Also the account link below needs to be updated to point to the proper url for editing account information */}
-        <li><a href="https://connectourkids.org/">Account</a></li> 
+        <li>{user ? user.name : ''}</li>
+        <li><a href="https://connectourkids.org/">connectourkids.org</a></li> 
         <li><a href="https://search.connectourkids.org/">People Search</a></li>
         <li><button className="nav__btn nav__btn--logout" onClick={() => logout({returnTo: redirectUri})}>Log Out</button></li>
       </ul>
@@ -30,15 +30,14 @@ const LoggedInDropdown = ({ open }) => {
   )
 }
 
-const MobileDropdown = ({ open, setOpen }) => {
-  const { logout } = useAuth0();
+const MobileDropdown = ({ open, setOpen, logout }) => {
   return (
     <div className={`nav__mobile-dropdown${open ? " open" : ""}`}>
       <ul>
         <li><NavLink to="/" exact className="subNav__link" onClick={() => setOpen(false)} >National Numbers</NavLink></li>
         <li><NavLink to="/personal" className="subNav__link" onClick={() => setOpen(false)} >My Achievements</NavLink></li>
         <li><NavLink to="/team" className="subNav__link" onClick={() => setOpen(false)} >My Team</NavLink></li>
-        <li><a href="https://connectourkids.org/" onClick={() => setOpen(false)} >Account</a></li>
+        <li><a href="https://connectourkids.org/" onClick={() => setOpen(false)} >connectourkids.org</a></li>
         <li><a href="https://search.connectourkids.org/" onClick={() => setOpen(false)} >People Search</a></li>
         <li><button className="nav__btn nav__btn--logout" onClick={() => logout({returnTo: redirectUri})}>Log Out</button></li>
       </ul>
@@ -47,8 +46,7 @@ const MobileDropdown = ({ open, setOpen }) => {
 }
 
 const NavBar = () => {
-  const { isAuthenticated, loginWithRedirect } = useAuth0();
-  const { user } = useContext(Auth0Context);
+  const { user, isAuthenticated, loginWithRedirect, logout } = useAuth0();
   const [open, setOpen] = React.useState(false); // Used for displaying and hiding the dropdown
 
   return (
@@ -71,12 +69,12 @@ const NavBar = () => {
                 <img className="nav_dropdownIconOpen" src={process.env.PUBLIC_URL + '/arrow-down-sign-to-navigate.svg'} alt="dropdown open" onClick={() => setOpen(true)} />
                 <img className="nav_dropdownIconClose" src={process.env.PUBLIC_URL + '/close-button.svg'} alt="dropdown close" onClick={() => setOpen(false)} />
                 <img className="nav__mobile-menu-btn" src={process.env.PUBLIC_URL + '/menu-button.svg'} alt="mobile menu" width="32" onClick={() => setOpen(!open)} />
-                <LoggedInDropdown open={open} />
+                <LoggedInDropdown open={open} logout={logout} user={user} />
               </>
             )}
         </div>
       </nav>
-      <MobileDropdown open={open} setOpen={setOpen} />
+      <MobileDropdown open={open} setOpen={setOpen} logout={logout} />
       {isAuthenticated && <LoggedInNav/>}
     </>
   );
